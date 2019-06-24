d145205ed94bf1cb8f7c30814130472790d50290
Joshua S Webb <josh10111991@gmail.com>
2019-06-24 02:33:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(person) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, person) {
+  `Happy ${holiday}, ${name}!`
+}
